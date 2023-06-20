5cb8a1d0f9b80d5485c47a005b613c540b5364ca
Zizan Ayub Al-Khalil <65456659+zizanayub@users.noreply.github.com>
2023-06-20 21:02:50 +0600

    Create Inserting Multiple Rows.sql

---
+++ 26.Inserting Multiple Rows/Inserting Multiple Rows.sql
@@ -0,0 +1,15 @@
+-- 01. Inserting Multiple Rows 101
+INSERT INTO shippers(name)
+VALUES ('shipper 1'),
+       ('shipper 2'),
+       ('shipper 3'); 
+
+
+
+-- 02. Exercise
+-- Insert 3 rows in the products table
+
+INSERT INTO products (name,quantity_in_stock,unit_price) VALUES
+					 ('product 1',2,2.98),
+                     ('product 2',3,6.78),
+                     ('product 3',4,8.98); 
